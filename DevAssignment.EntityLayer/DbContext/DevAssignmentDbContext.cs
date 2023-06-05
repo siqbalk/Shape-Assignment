0856ad422e558a1386c9f04e67be4a7448bfa7d8
Syed Iqbal <syed.iqbal@mparsec.com>
2023-06-05 09:18:25 +0500

    modify db context class

--- DevAssignment.EntityLayer/DbContext/DevAssignmentDbContext.cs
+++ DevAssignment.EntityLayer/DbContext/DevAssignmentDbContext.cs
@@ -9,19 +9,9 @@ namespace EntityLayer.DbContext
 {
     public class DevAssignmentDbCOntext : IdentityDbContext<User>
     {
-        private readonly IConfiguration _configuration;
-
-        public DevAssignmentDbCOntext(DbContextOptions<DevAssignmentDbCOntext> options, IConfiguration configuration) : base(options)
+        public DevAssignmentDbCOntext(DbContextOptions<DevAssignmentDbCOntext> options) : base(options)
         {
-            _configuration = configuration;
             Database.SetCommandTimeout(120000);
-        }
-
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Data Source=MPARSECPC;Initial Catalog=DevAssignmentDb;Integrated Security = SSPI;");
-            optionsBuilder.EnableSensitiveDataLogging();
         }
     }
 
